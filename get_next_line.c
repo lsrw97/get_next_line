7ab11527ad447044d18456d8fb2fcfb6883d40d3
42 <home42@Eugens-MacBook-Air.local>
2023-01-27 14:21:12 +0000

    asdf

--- get_next_line.c
+++ get_next_line.c
@@ -16,20 +16,163 @@
 #include <stdlib.h>
 #include <string.h>
 
-// what line to read
-// where to read from - fd
-//
+// // what line to read
+// // where to read from - fd
+// //
 
-// Hello World!/nMy name is 42/nI love Burgers!/0
-// BUFFER_SIZE = 5;
+// // Hello World!/nMy name is 42/nI love Burgers!/0
 
-// Hello World!/nMy
-//
+// // Hello World!/nMy
+// //
 
-// check if it has a /n character in string
-// if ture -> trunc temp
+// // check if it has a /n character in string
+// // if ture -> trunc temp
 
-//
+// //
+#define BUFFER_SIZE 5
+
+
+
+// void	strcopy(char *str1, char *str2)
+// {
+// 	int i;
+
+// 	i = -1;
+// 	while(++i < ft_strlen(str2))
+// 	{
+// 		str1[i] = str2[i];
+// 	}
+// }
+
+// int getIndex(char *buffer)
+// {
+// 	int	i;
+
+// 	i = -1;
+// 	while (buffer[++i])
+// 	{
+// 		if (buffer[i] == '\n')
+// 			return (i);
+// 	}
+// 	return (i);
+// }
+
+// int	buffereol(char *buffer)
+// {
+// 	int	i;
+// 	int j;
+
+// 	j = 0;
+// 	i = -1;
+// 	while (buffer[++i])
+// 	{
+// 		// printf("Bufferol: %s\n\n", buffer);
+// 		if (buffer[i] == '\n')
+// 		{
+// 			while (buffer[++i])
+// 				buffer[j++] = buffer[i];
+// 			// printf("MyBuffer: %s", buffer);
+// 			return (1);
+// 		}
+// 	}
+// 	return (0);
+// }
+
+// int	checkeol(char *buffer, char *line, char *eol)
+// {
+// 	char *temp;
+// 	int i;
+// 	int j;
+// 	int k;
+
+// 	k = 0;
+// 	i = -1;
+// 	// printf("Index: %d\n", getIndex(buffer));
+// 	temp = malloc(ft_strlen(line) + ft_strlen(buffer));
+// 	j = ft_strlen(line);
+// 	strcopy(temp, line);
+// 	while (buffer[++i])
+// 	{
+// 		if (buffer[i] != '\0' && buffer[i] != '\n')
+// 			{temp[j++] = buffer[i];
+// 					// printf("\ntemp:%s\n", temp);
+// }
+// 		else
+// 		{
+// 			if (buffer[i] == '\n')
+// 				{
+// 					buffer[i] = 0;
+// 					while (buffer[++i])
+// 					{
+// 						buffer[k++] = buffer[i];
+// 						buffer[i] = '\0';
+// 					}
+// 					buffer[i] = '\0';
+// 					// printf("\nMybuffer: %s\n", buffer);
+// 					free(line);
+// 					eol[0] = 1;
+// 					temp[j] = '\n';
+// 					temp[j + 1] = '\0';
+// 					line = malloc(ft_strlen(temp));
+// 					strcopy(line, temp);
+// 					// printf("\nline: %s", line);
+// 					return (1);
+// 				}
+// 		}
+// 	}
+// 	free(line);
+// 	eol[0] = 0;
+// 	temp[j] = '\0';
+// 	// printf("i was here: %s\n", line);
+// 	line = malloc(ft_strlen(temp) + 1);
+// 	// printf("%d\n", ft_strlen(temp));
+// 	strcopy(line, temp);
+// 	line[j] = '\0';
+// 	return (0);
+// }
+
+// void	fillBuffer(char *str)
+// {
+
+// }
+
+// char	*get_next_line(int fd)
+// {
+// 	char *temp1;
+// 	char *temp2;
+// 	static int linesize;
+// 	int i;
+// 	char *line;
+// 	static char *buffer;
+// 	static char *eol;
+
+// 	eol = malloc(1);
+// 	eol[0] = 0;
+// 	if (!buffer)
+// 	// hello
+// 		buffer = malloc(read(fd, buffer, BUFFER_SIZE) + 1);
+	
+// 	line = malloc(ft_strlen(buffer));
+// 	//	if there isn't a \n in a buffer string
+// 	if(!eol[0])
+// 	{
+// 		if(!read(fd, buffer, BUFFER_SIZE))
+// 			return (NULL);
+// 	}
+// 	buffer[ft_strlen(buffer)] = '\0';
+// 	while (!checkeol(buffer, line, eol))
+// 	{
+// 			if(!read(fd, buffer, BUFFER_SIZE))
+// 				break;
+// 			// BUFFER = hel__\0 -> hel\0\0\0
+// 			buffer[BUFFER_SIZE + 1] = '\0';
+// 	}
+// 	// printf("eol: %d", eol[0]);
+// 	printf("%s", line);
+
+// 	return (line);
+// }
+
 #define BUFFER_SIZE 5
 
 size_t	ft_strlen(const char *s)
@@ -39,129 +182,127 @@ size_t	ft_strlen(const char *s)
 	i = 0;
 	if (s == NULL)
 		return (0);
-	while (s[i] != '\0')
-	{
-		if (s[i] == '\n')
-			return(++i);
+	while (s[i])
 		i++;
-	}
 	return (i);
 }
 
-void	strcopy(char *str1, char *str2)
+int getId(char *buffer, char c)
+{
+	int i;
+
+	i = 0;
+	while (buffer[i] != c)
+		i++;
+	return (i);
+}
+
+int	strcopy(char *dst, char *src)
 {
 	int i;
 
 	i = -1;
-	while(++i < ft_strlen(str2))
-	{
-		str1[i] = str2[i];
-	}
-}
-
-int getIndex(char *buffer)
-{
-	int	i;
-
-	i = -1;
-	while (buffer[++i])
-	{
-		if (buffer[i] == '\n')
-			return (i);
-	}
+	while (src[++i])
+		dst[i] = src[i];
 	return (i);
 }
 
-int	buffereol(char *buffer)
+void	concatbuffertemp(char *buffer, char *temp, int id)
 {
-	int	i;
-
-	i = -1;
-	while (buffer[++i])
-	{
-		if (buffer[i] == '\n')
-			buffer = "My";
-			return (1);
-	}
-	return (0);
-}
-
-int	checkeol(char *buffer, char *line)
-{
-	char *temp;
 	int i;
 	int j;
 
-	i = -1;
-	printf("Index: %d\n", getIndex(buffer));
-	temp = malloc(ft_strlen(line) + ft_strlen(buffer));
-	j = ft_strlen(line);
-	strcopy(temp, line);
-	while (buffer[++i] != '\0')
+	j = -1;
+	i = ft_strlen(temp);
+	while (++j <= id)
 	{
-		if (buffer[i] != '\0' && buffer[i] != '\n')
-			{temp[j++] = buffer[i];
-					// printf("\ntemp:%s\n", temp);
-}
-		else
-		{
-			if (buffer[i] == '\n')
-				{
-					temp[j] == '\n';
-					free(line);
-					line = malloc(ft_strlen(temp));
-					strcopy(line, temp);
-					printf("\nline: %s\n", line);
-					return (1);
-				}
-			if (buffer[i] == '\0')
-				{
-				return (2);
-				}
-
-		}
-
+		temp[i++] = buffer[j];
 	}
-	free(line);
-	line = malloc(ft_strlen(temp));
-	strcopy(line, temp);
-	return (0);
+	temp[i] = '\0';
+
+}
+
+void	addBufferStringToLine(char *buffer, char *line, int state)
+{
+	int id;
+	char *temp;
+
+	temp = malloc(ft_strlen(buffer) + ft_strlen(line));
+	if (line)
+		strcopy(temp, line);
+
+	id = getId(buffer, state);
+	concatbuffertemp(buffer, temp, id);
+	if(line)
+		free(line);
+	line = malloc(ft_strlen(temp) + 1);
+	line[ft_strlen(temp)] = '\0';
+}
+
+void	convertBuffer(char *buffer)
+{
+	int i;
+	int j;
+
+	j = 0;
+	i = 0;
+	while (buffer[i] != '\n')
+		i++;
+	while(buffer[++i])
+		buffer[j++] = buffer[i];
+	buffer[j] = '\0';
 }
 
 char	*get_next_line(int fd)
 {
-	char *temp1;
-	char *temp2;
-	static int linesize;
-	int bufsize = 5;
-	int i;
-	char *line;
 	static char *buffer;
+	char *line;
+	int i;
+	int size;
 
-	if (!buffer)
-		buffer = malloc(read(fd, buffer, BUFFER_SIZE) + 1);
-		line = malloc(ft_strlen(buffer));
-		read(fd, buffer, BUFFER_SIZE);
-	buffer[ft_strlen(buffer)] = '\0';
-	while (!checkeol(buffer, line))
+	i = -1;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	size = read(fd, buffer, BUFFER_SIZE);
+	while (size <= BUFFER_SIZE)
+	buffer[size++] = '\0';
+	buffer[BUFFER_SIZE] = '\0';
+	printf("%zu", ft_strlen(buffer));
+	line = malloc(ft_strlen(buffer));
+
+	while(1)
 	{
-		printf("\nBuffer: %s\n", buffer);
-		if(buffereol(buffer))
-			read(fd, buffer, BUFFER_SIZE);
-		// printf("%s", line);
-		// printf("\nline: %s\n", line);
+		if(buffer[++i] == '\n')
+		{
+			addBufferStringToLine(buffer, line, 10);
+			convertBuffer(buffer);
+			break;
+		}
+		else if(buffer[i] == '\0')
+		{
+			addBufferStringToLine(buffer, line, 0);
+			buffer[0] = '\0';
+			break;
+		}
 	}
+	// printf("%d", size);
 	return (line);
 }
 
+
+
 int main (void)
 {
-	static char buffer[BUFFER_SIZE];
 	int	fds;
 
 	fds = open("test.txt", O_RDONLY);
+
 	get_next_line(fds);
-	get_next_line(fds);
+
+
+	// get_next_line(fds);
+	// printf("%s", get_next_line(fds));
+
 
 	// printf("%d", fds);
 		// get_next_line(fds);
